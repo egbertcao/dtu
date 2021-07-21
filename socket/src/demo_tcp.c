7df2bd907656b509a6bac0aec663b6903c0a9b27
albert <egbert123@sina.com>
2021-07-21 11:11:33 +0800

    tcp 重连，待测试

--- socket/src/demo_tcp.c
+++ socket/src/demo_tcp.c
@@ -13,18 +13,17 @@ static BOOL bWorkerRun = FALSE;
 static BOOL bMonitorRun = FALSE;
 extern dtu_config_t g_dtu_config;
 
+static BOOL TCP_CONNECTION = FALSE;
+
 void dtu_tcp_send(char *message)
 {
 	int bytes = send(sock_fd, message, strlen(message), 0);
 	printf("bytes = %d", bytes);
 }
 
-void tcp_worker_thread(void)
+void dtu_tcp_connect()
 {
-	OC_UART_LOG_Printf("[%s] tcp Worker Start!\n", __func__);
-	int bytes =0;
 	int res=-1;
-	char recvbuf[64]={0};
 	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
@@ -40,56 +39,42 @@ void tcp_worker_thread(void)
 				break;
 			}
 		}
-		OSATaskSleep(200); 
+		OSATaskSleep(2000); 
 	}
 	
     OC_UART_LOG_Printf("[%s] connect Success\n", __func__);
+	TCP_CONNECTION = TRUE;
+}
+
+// 只接收数据
+void tcp_worker_thread(void)
+{
+	int bytes =0;
+	char recvbuf[64]={0};
+	OC_UART_LOG_Printf("[%s] tcp Worker Start!\n", __func__);
 	while (bWorkerRun)
 	{
-		bytes = recv(sock_fd, recvbuf, sizeof(recvbuf), 0);
-		if(bytes <= 0) {
-			OC_UART_LOG_Printf("[%s]: socket disconnect\n", __func__);
-			bWorkerRun = FALSE;
-			break;
+		if(TCP_CONNECTION) {
+			bytes = recv(sock_fd, recvbuf, sizeof(recvbuf), 0);
+			if(bytes <= 0) {
+				OC_UART_LOG_Printf("[%s]: socket disconnect\n", __func__);
+				TCP_CONNECTION = FALSE;
+			}
+			OC_UART_LOG_Printf("[%s] recvbuf:%s,bytes:%d", __func__,recvbuf,bytes);
+			received_from_server(recvbuf, bytes, TRANS_TCP);
 		}
-		
-		OC_UART_LOG_Printf("[%s] recvbuf:%s,bytes:%d", __func__,recvbuf,bytes);
-		received_from_server(recvbuf, bytes, TRANS_TCP);
 	}
 }
 
 void tcp_monitor_thread(void)
 {
 	OC_UART_LOG_Printf("[%s] tcp Monitor Start!\n", __func__);
-	void *TcpWorkTaskStack;
-	TcpWorkTaskStack=malloc(4096);
-	if(TcpWorkTaskStack == NULL)
-	{
-		return;
-	}
-	bWorkerRun = TRUE;
-	if(OSATaskCreate(&tcpWorkerRef,
-	                 TcpWorkTaskStack,
-	                 4096,80,(char*)"tcpWorkerTask",
-	                 tcp_worker_thread, NULL) != 0)
-	{
-		return;
-	}
-	
 	while (bMonitorRun)
 	{
-		if(bWorkerRun == FALSE){
+		if(TCP_CONNECTION == FALSE){
 			int netstatus = OC_GetNetStatus();
 			if(netstatus == 1) { // 网络恢复后再重新连接
-				OSATaskDelete(tcpWorkerRef); 
-				bWorkerRun = TRUE;
-				if(OSATaskCreate(&tcpWorkerRef,
-								TcpWorkTaskStack,
-								4096,80,(char*)"tcpWorkerTask",
-								tcp_worker_thread, NULL) != 0)
-				{
-					return;
-				}
+				dtu_tcp_connect();
 			}
 		}
 		OSATaskSleep(2000); //每10s查询一次TCP状态
@@ -109,6 +94,21 @@ void customer_app_tcp_start(void)
 	                 TcpMonitorTaskStack,
 	                 4096,80,(char*)"tcpMonitorTask",
 	                 tcp_monitor_thread, NULL) != 0)
+	{
+		return;
+	}
+
+	void *TcpWorkTaskStack;
+	TcpWorkTaskStack=malloc(4096);
+	if(TcpWorkTaskStack == NULL)
+	{
+		return;
+	}
+	bWorkerRun = TRUE;
+	if(OSATaskCreate(&tcpWorkerRef,
+	                 TcpWorkTaskStack,
+	                 4096,80,(char*)"tcpWorkerTask",
+	                 tcp_worker_thread, NULL) != 0)
 	{
 		return;
 	}
